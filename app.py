fb79b2f938031118eff5f6b4b9570bab274211ec
Raghav Kavimandan <raghavkavimandan23@gmail.com>
2021-09-08 23:00:44 +0530

    added deployment functionality

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, app, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    model = joblib.load('my_model.pkl')
+
+    if request.method == 'POST':
+        my_features = [x for x in request.form.values()]
+
+    
+    final_features = np.asarray(my_features).reshape(1,-1)
+    preds = model.predict(final_features)
+    output = preds[0]
+
+    return render_template('display.html', output=output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
